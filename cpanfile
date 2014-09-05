5bfec51fa77d8ab2ffcb13f50e29a89759565da9
Jan Henning Thorsen <jhthorsen@cpan.org>
2014-09-05 14:33:21 +0200

    Use git-ship

---
+++ cpanfile
@@ -0,0 +1,5 @@
+# You can install this projct with curl -L http://cpanmin.us | perl - https://github.com/jhthorsen/applify/archive/master.tar.gz
+requires "Cwd" => "3.31",
+requires "File::Basename" => "2.70",
+requires "Getopt::Long" => "2.38",
+test_requires "Test::More" => "0.88";
--- lib/Applify.pm
+++ lib/Applify.pm
@@ -6,7 +6,7 @@ Applify - Write object oriented scripts with ease
 
 =head1 VERSION
 
-0.06
+0.07
 
 =head1 DESCRIPTION
 
@@ -91,7 +91,7 @@ use constant SUB_NAME_IS_AVAILABLE => $INC{'App/FatPacker/Trace.pm'}
   ? 0    # this will be true when running under "fatpack"
   : eval 'use Sub::Name; 1' ? 1 : 0;
 
-our $VERSION = '0.06';
+our $VERSION = '0.07';
 our $PERLDOC = 'perldoc';
 my $ANON = 1;
 
