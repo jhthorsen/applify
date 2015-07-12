04658caf70bf7f038ae75c7a677c64b7cb909e03
Jan Henning Thorsen <jhthorsen@cpan.org>
2015-07-12 11:32:25 +0200

    Released version 0.11
     - Bump minimum required Perl version to 5.10.1
     - AUTOLOAD does not work on Perl 5.10.1

--- lib/Applify.pm
+++ lib/Applify.pm
@@ -6,7 +6,7 @@ Applify - Write object oriented scripts with ease
 
 =head1 VERSION
 
-0.10
+0.11
 
 =head1 DESCRIPTION
 
@@ -91,7 +91,7 @@ use constant SUB_NAME_IS_AVAILABLE => $INC{'App/FatPacker/Trace.pm'}
   ? 0    # this will be true when running under "fatpack"
   : eval 'use Sub::Name; 1' ? 1 : 0;
 
-our $VERSION = '0.10';
+our $VERSION = '0.11';
 our $PERLDOC = 'perldoc';
 my $ANON = 1;
 
